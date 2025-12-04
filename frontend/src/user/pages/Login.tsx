a3de4308e585dbf82033ca1635738ac2b1e0267e
Jason Amar <jason.amar2@gmail.com>
2025-12-04 18:58:23 -0500

    fixed login bug

--- frontend/src/user/pages/Login.tsx
+++ frontend/src/user/pages/Login.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import Button from "../../shared/components/FormElements/Button";
-import Input from "../../shared/components/FormElements/Input";
+import Button from '../../shared/components/FormElements/Button';
+import Input from '../../shared/components/FormElements/Input';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
-} from "../../shared/util/validators";
-import { useForm } from "../../shared/hooks/form-hook";
-import "./Login.css";
-import Card from "../../shared/components/UIElements/Card";
-import { useAuth } from "../../shared/context/auth-context";
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import './Login.css';
+import Card from '../../shared/components/UIElements/Card';
+import { useAuth } from '../../shared/context/auth-context';
 
 const Login = () => {
   const { login } = useAuth();
@@ -18,31 +18,25 @@ const Login = () => {
 
   const [formState, inputHandler, setFormData] = useForm(
     {
-      email: { value: "", isValid: false },
-      password: { value: "", isValid: false },
-      name: { value: "", isValid: false, ignore: true },
+      email: { value: '', isValid: false },
+      password: { value: '', isValid: false },
+      name: { value: '', isValid: false, ignore: true },
     },
     false
   );
 
   const switchModeHandler = () => {
-    if (!isLogin) {
-      setFormData(
-        {
-          ...formState.inputs,
-          name: { ...formState.inputs.name, ignore: true },
-        },
-        formState.isValid
-      );
-    } else {
-      setFormData(
-        {
-          ...formState.inputs,
-          name: { ...formState.inputs.name, ignore: false },
-        },
-        false
-      );
-    }
+    const formValidity = !isLogin
+      ? formState.inputs.email.isValid && formState.inputs.password.isValid
+      : formState.isValid;
+
+    setFormData(
+      {
+        ...formState.inputs,
+        name: { ...formState.inputs.name, ignore: !isLogin },
+      },
+      formValidity
+    );
     setIsLogin((prevMode) => !prevMode);
   };
 
@@ -54,46 +48,46 @@ const Login = () => {
   };
 
   return (
-    <Card className="authentication">
+    <Card className='authentication'>
       <h2>Login Required</h2>
-      <form className="signin-form" onSubmit={loginSubmitHandler}>
+      <form className='signin-form' onSubmit={loginSubmitHandler}>
         {!isLogin && (
           <Input
-            id="name"
-            element="input"
-            type="text"
-            label="Name"
+            id='name'
+            element='input'
+            type='text'
+            label='Name'
             validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid name."
+            errorText='Please enter a valid name.'
             onInput={inputHandler}
             initialValue={formState.inputs.name.value}
             initialValid={formState.inputs.name.isValid}
           />
         )}
         <Input
-          id="email"
-          element="input"
-          type="text"
-          label="Email"
+          id='email'
+          element='input'
+          type='text'
+          label='Email'
           validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email."
+          errorText='Please enter a valid email.'
           onInput={inputHandler}
         />
         <Input
-          id="password"
-          element="input"
-          type="text"
-          label="Password"
+          id='password'
+          element='input'
+          type='text'
+          label='Password'
           validators={[VALIDATOR_MINLENGTH(10)]}
-          errorText="Please enter a valid password (min 10 characters)."
+          errorText='Please enter a valid password (min 10 characters).'
           onInput={inputHandler}
         />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLogin ? "LOGIN" : "SIGNUP"}
+        <Button type='submit' disabled={!formState.isValid}>
+          {isLogin ? 'LOGIN' : 'SIGNUP'}
         </Button>
       </form>
       <Button inverse onClick={switchModeHandler}>
-        {`SWITCH TO ${isLogin ? "SIGNUP" : "LOGIN"} `}
+        {`SWITCH TO ${isLogin ? 'SIGNUP' : 'LOGIN'} `}
       </Button>
     </Card>
   );
